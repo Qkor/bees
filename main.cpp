e4014383ffd238c08cf25f4972b78160f10734ad
jk <jan.kukorowski@wp.pl>
2024-06-01 13:29:15 +0200

    reading input

--- main.cpp
+++ main.cpp
@@ -16,8 +16,24 @@ void secondThread(){
 int main(int argc, char** argv){
 	MPI_Init(&argc, &argv);
 
-	int process_id;
+	int process_id, number_of_processes;
 	MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+	MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
+
+	if(argc != 3) {
+		if(!process_id)
+			cout << "Usage: mpiexec -n Number_of_bees " << argv[0] << " Number_of_reeds Number_of_flowers\n";
+		MPI_Finalize();
+		return 1;
+	}
+
+	// number of bees
+	int P = number_of_processes;
+	// number of reeds
+	int T = std::atoi(argv[1]);
+	// number of flowers
+	int K = std::atoi(argv[2]);
+
 
 	thread first(firstThread);
 	thread second(secondThread);
